5c9b358cba305bb0a54ad96d3be0b4c815dba1e0
705875441 <55942744+705875441@users.noreply.github.com>
2019-12-15 17:36:51 +0800

    Update A.java

--- Test2/src/main/java/com/jt/A.java
+++ Test2/src/main/java/com/jt/A.java
@@ -1,16 +1,4 @@
 package com.jt;
 
 public class A {
-	public void a() {
-		System.out.println();
-	}
-	public void b() {
-		System.out.println("aa"); 
-	}
-	public void c() {
-		System.out.println("aa"); 
-	}
-	public void d() {
-		System.out.println("aaa"); 
-	}
 }
